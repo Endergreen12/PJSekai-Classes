04f73f06caebaa79120cb83a64f29a9208dd56f5
ender <ender@DESKTOP-6CB1D4G>
2024-10-25 17:25:58 +0900

    プロジェクト ファイルを追加します。

---
+++ PJSekai-Classes/Sekai.cs
@@ -0,0 +1,27 @@
+﻿using MessagePack;
+
+// Version 4.0.0
+namespace Sekai
+{
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class AssetBundleInfo
+    {
+        public String version;
+        public Dictionary<String, AssetBundleElement> bundles;
+    }
+
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class AssetBundleElement
+    {
+        public String bundleName;
+        public String cacheFileName;
+        public String cacheDirectoryName;
+        public String hash;
+        public String category;
+        public UInt32 crc;
+        public Int64 fileSize;
+        public String[] dependencies;
+        public String[] paths;
+        public Boolean isBuiltin;
+    }
+}
